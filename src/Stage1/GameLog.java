66603437fae22ed932653841b53e2dd7caf0d1e0
amirelkased <amirelkased8558@gmail.com>
2022-09-28 15:17:55 +0200

    First Stage

---
+++ src/Stage1/GameLog.java
@@ -0,0 +1,19 @@
+package Stage1;
+
+public class GameLog {
+
+    public static void Stage1() {
+        System.out.println("""
+                The secret code is prepared: ****.
+                                
+                Turn 1. Answer:
+                1234
+                Grade: None.
+                                
+                Turn 2. Answer:
+                9876
+                Grade: 4 bulls.
+                Congrats! The secret code is 9876.
+                """);
+    }
+}
---
+++ src/Stage1/Main.java
@@ -0,0 +1,13 @@
+package Stage1;
+
+import java.util.Scanner;
+
+import static Stage1.GameLog.*;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Stage1();
+    }
+}
