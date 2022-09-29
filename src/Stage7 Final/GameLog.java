e40248195e41d406f711a9783f815e3414ffecb0
amirelkased <amirelkased8558@gmail.com>
2022-09-29 21:16:19 +0200

    Add final stage which handled exceptions

---
+++ src/Stage7 Final/GameLog.java
@@ -0,0 +1,116 @@
+package bullscows;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GameLog {
+    private static StringBuilder secret_num;
+
+    public static boolean acceptLength(int length, int noOfSymbol) {
+        if (noOfSymbol < length) {
+            System.out.println("Error: it's not possible to generate a code with a length of 6 with 5 unique symbols.");
+
+        }
+        secret_num = generateRandomNumber(length, noOfSymbol);
+        if (secret_num == null) {
+            return true;
+        }
+        System.out.print("The secret is prepared: " + "*".repeat(length)
+                + " (0-9, ");
+        if (noOfSymbol - 10 == 0) {
+            System.out.println("a).");
+        } else {
+            System.out.print("a-" + (char) (96 + (noOfSymbol - 10)) + ").\n");
+        }
+        return false;
+    }
+
+    public static boolean newTurn(String guess) {
+        char[] secretNum = String.valueOf(secret_num).toCharArray();
+        char[] guessNum = guess.toCharArray();
+        int cntBull = 0;
+        int cntCow = 0;
+        for (int i = 0; i < secretNum.length; i++) {
+            if (secretNum[i] == guessNum[i]) {
+                ++cntBull;
+                guessNum[i] = '-';
+            }
+            for (int j = 0; j < guessNum.length; j++) {
+                if (secretNum[i] == guessNum[j]) {
+                    if (i == j) {
+                        ++cntCow;
+                    }
+                    guessNum[j] = '-';
+                    break;
+                }
+            }
+        }
+        return calBullCow(secretNum.length, cntBull, cntCow);
+    }
+
+    public static void formatStage1() {
+        System.out.println("""
+                The secret code is prepared: ****.
+                                
+                Turn 1. Answer:
+                1234
+                Grade: None.
+                                
+                Turn 2. Answer:
+                9876
+                Grade: 4 bulls.
+                Congrats! The secret code is 9876.
+                """);
+    }
+
+    public static boolean calBullCow(int lengthSecretNum, int cntBull, int cntCow) {
+        System.out.print("Grade: ");
+        if (cntBull > 0) {
+            System.out.printf("%d bull", cntBull);
+            if (cntBull > 1) System.out.print("s");
+        }
+
+        if (cntBull % 4 != 0 && cntCow > 0) {
+            System.out.print(" and ");
+        }
+
+        if (cntCow > 0) {
+            System.out.printf("%d cow", cntCow);
+            if (cntBull > 1) System.out.print("s");
+        }
+
+        if (cntBull == 0 && cntCow == 0) {
+            System.out.print("None.");
+        }
+        System.out.println();
+        if (cntBull == lengthSecretNum) {
+            System.out.print("Congratulations! You guessed the secret code.");
+            return true;
+        }
+        return false;
+    }
+
+    public static StringBuilder generateRandomNumber(int length, int noOfSymbol) {
+        if (length > 36) {
+            System.out.println("Error: can't generate a secret number with a length " +
+                    "of 11 because there aren't enough unique digits.");
+            return null;
+        } else {
+            StringBuilder generateNum;
+            String letter = "0123456789abcdefghijklmnopqrstuvwxyz";
+            do {
+                generateNum = new StringBuilder();
+                for (int i = 0; i < length; ++i) {
+                    generateNum.append(letter.charAt((int) (Math.random() * 10) % noOfSymbol));
+                }
+            } while (!checkUnique(generateNum));
+            return generateNum;
+        }
+    }
+
+    private static boolean checkUnique(StringBuilder num) {
+        String number = String.valueOf(num);
+        List<String> arr = Arrays.stream(number.split("")).distinct().toList();
+        return arr.size() == number.length();
+    }
+}
---
+++ src/Stage7 Final/Main.java
@@ -0,0 +1,67 @@
+package bullscows;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import static bullscows.GameLog.*;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    final static Pattern pattern = Pattern.compile("[^0-9]");
+
+    public static void main(String[] args) {
+        if (takeInput()) {
+            query();
+        }
+    }
+
+    private static boolean takeInput() {
+        int length;
+        int noOfChar;
+        do {
+            // first i/p
+            System.out.println("Input the length of the secret code:");
+            String len = scanner.nextLine();
+            if (checkInput(len)) {
+                System.out.printf("Error: \"%s\" isn't a valid number.", len);
+                return false;
+            }
+            length = Integer.parseInt(len);
+            if (length < 1) {
+                System.out.println("Error: is not logic!");
+                return false;
+            }
+            // second i/p
+            System.out.println("Input the number of possible symbols in the code:");
+            String chars = scanner.nextLine();
+            if (checkInput(chars)) {
+                System.out.printf("Error: \"%s\" isn't a valid number.", chars);
+                return false;
+            }
+            noOfChar = Integer.parseInt(chars);
+            // not of chars
+            if (noOfChar < length) {
+                System.out.printf("Error: it's not possible to generate a code with " +
+                        "a length of %d with %d unique symbols.\n", length, noOfChar);
+                return false;
+            } else if (noOfChar > 36) {
+                System.out.println("Error: maximum number of possible symbols in the code is 36 (0-9, a-z).");
+                return false;
+            }
+        } while (acceptLength(length, noOfChar));
+        return true;
+    }
+
+    private static void query() {
+        for (int i = 1; ; i++) {
+            System.out.printf("Turn %d:\n", i);
+            if (newTurn(scanner.next())) {
+                break;
+            }
+        }
+    }
+
+    private static boolean checkInput(String x) {
+        return pattern.matcher(x).find();
+    }
+}
